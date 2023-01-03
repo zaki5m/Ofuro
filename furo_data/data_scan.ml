d32a6abd41fc48988730c5a1f699bd55fca0095e
山崎陽介 <yamazakiyousuke@yamazakiyousukenoMacBook-Pro.local>
2023-01-03 17:48:48 +0900

    overwrite

--- furo_data/data_scan.ml
+++ furo_data/data_scan.ml
@@ -1,7 +1,7 @@
 
-
+(*
 let _ = 
-  let file = open_in "correct_data_total.dat" in 
+  let file = open_in "totaldata.txt" in 
   let rec loop tmp = 
     let a, b, c, d, e = Scanf.sscanf (input_line file) "%d %f %f %f %f" (fun a b c d e -> (a,b,c,d,e)) in 
     let tmp = (a,b,c,d,e)::tmp in
@@ -13,14 +13,13 @@ let _ =
     | [] -> tmp2
     | h::t -> let (a,b,c,d,e) = h in 
               let tmp2 = 
-                if  a <= 15 then 
+                if a <= 9 || a > 12 then 
                   tmp2
                 else
                   (a,b,c,d,e)::tmp2
               in
               loop2 t tmp2
   in
-  (*
   let lst2 = loop2 lst [] in 
   let m = List.length lst2 in 
   Printf.printf "%d\n" m;
@@ -35,6 +34,7 @@ let _ =
   in
   loop3 (m-1)
 *)
+(*
  let lst2 = loop2 lst [[];[];[];[]] in
  let lsta = List.nth lst2 0 in 
  let lstb = List.nth lst2 1 in 
@@ -60,3 +60,58 @@ let _ =
  let c = List.length (List.filter (fun x -> x = 3.0) lstd) in 
  let d = List.length (List.filter (fun x -> x = 4.0) lstd) in 
  Printf.printf "d:%d %d %d %d\n"a b c d; flush stdout;
+*)
+(*
+let _ = 
+  let file = open_in "same_count.txt" in 
+  let rec loop tmp tmp2 tmp3 now = 
+    let ip_s = input_line file in 
+    if ip_s = "finish" then 
+      let tmp2 = tmp2 + 1 in 
+      try loop tmp tmp2 tmp3 now 
+      with End_of_file -> close_in file; (tmp,tmp2,tmp3)
+    else
+      let tmp = tmp+1 in
+      let tmp3 = if now = tmp2 then tmp3 else tmp3 + 1 in 
+      let now = tmp2 in 
+      try loop tmp tmp2 tmp3 now 
+      with End_of_file -> close_in file; (tmp,tmp2,tmp3)
+  in
+  let (lst1,lst2,lst3) = loop 0 0 0 (-1) in
+  Printf.printf "%d %d %d\n" lst1 lst2 lst3;
+*)
+
+let _ = 
+  let file = open_in "total.txt" in 
+  let rec loop tmp = 
+    let _, _, a, b, c, d, e, f, g, h = Scanf.sscanf (input_line file) "%s %s %s %s %s %s %s" (fun a b c d e f g h i j -> (a,b,c,d,e,f,g,h,i,j)) in 
+    let tmp = (a,b,c,d,e,f,g,h)::tmp in
+    try loop tmp 
+    with End_of_file -> close_in file; tmp
+  in
+  let lst = loop [] in
+  let rec loop2 tmp1 = match tmp1 with
+    | [] -> ()
+    | h::t -> let (a,b,c,d,e,f,g,h) = h in 
+              let a = String.sub a 1 (String.len a - 1) in 
+              let b1 = String.sub b 5 12 in
+              let b2 = String.sub b 21 28 in 
+              let b3 = String.sub b 31 (String.len b - 1) in
+              let c = String.sub c 1 (String.len c - 1) in 
+              let d1 = String.sub d 5 12 in
+              let d2 = String.sub d 21 28 in 
+              let d3 = String.sub d 31 (String.len d - 1) in
+              let e = String.sub e 1 (String.len e - 1) in 
+              let f1 = String.sub f 5 12 in
+              let f2 = String.sub f 21 28 in 
+              let f3 = String.sub f 31 (String.len f - 1) in
+              let g = String.sub g 1 (String.len g - 1) in 
+              let h1 = String.sub h 5 12 in
+              let h2 = String.sub h 21 28 in 
+              let h3 = String.sub h 31 (String.len h - 1) in
+              let total = a ^ "," ^ b1 ^ "," ^ b2 ^ "," ^ b3 ^ "," ^ a ^ "," ^ d1 ^ "," ^ d2 ^ "," ^ d3 ^ "," ^ a ^ "," ^ f1 ^ "," ^ f2 ^ "," ^ f3 ^ "," ^ a ^ "," ^ h1 ^ "," ^ h2 ^ "," ^ h3 ^ "\n" in
+              Printf.printf total;
+              loop2 t
+  in
+  loop2 lst
+  
--- furo_data/gragh_create.ml
+++ furo_data/gragh_create.ml
@@ -1,17 +1,17 @@
 let make_gragh lst = 
   let m = List.length lst in 
-  let rec loop i a2 b2 tmp =
+  let rec loop i a2 b2 c2 tmp =
     let (a,b,c) = List.nth lst i in
     let tmp = 
-      if ((a2 *. b *. b *. b) > c && b < 0.) || (b2 *. b *.b > c) && b > 0. then 
+      if (*((a2 *. b *. b *. b) > c && b < 0.) ||*) (((-1.) *. b2 *. b *.b +. c2 < c) && b > 0.) || c > 0. then 
         tmp + 1
       else
         tmp
     in
     let _ = 
       if i = 0 then 
-        if tmp >= 142 then 
-          (Printf.printf "%f %f %d\n" a2 b2 tmp; flush stdout;)
+        if tmp >= 700 && tmp <= 800 then 
+          (Printf.printf "%f %f %f %d\n" a2 b2 c2 tmp; flush stdout;)
         else if tmp > 0 then 
           (*Printf.printf "%f,%f,%d\n" a2 b2 tmp; flush stdout;*)
           ()
@@ -22,19 +22,31 @@ let make_gragh lst =
     in
     if i = 0 then 
       if a2 >= 10000000.0 then
-        if b2 >= 1000.0 then
-          ()
+        if b2 >= 10000000.0 then
+          if c2 >= 0. then
+            ()
+          else
+            loop (m-1) a2 b2 (c2 +. 10.) 0 
         else
-          loop (m-1) a2 (b2 +. 1.0) 0 
+          if c2 >= 0. then
+            loop (m-1) a2 (b2 +. 1000.) 0. 0 
+          else
+          loop (m-1) a2 b2 (c2 +. 10.) 0 
       else
-        if b2 >= 1000.0 then
-          loop (m-1) (a2 +. 10000.) (0.0) 0
+        if b2 >= 10.0 then
+          if c2 >= 10000. then
+            loop (m-1) (a2 +. 100000.) (0.0) 0. 0
+          else
+          loop (m-1) a2 b2 (c2 +. 100.) 0 
         else
-          loop (m-1) a2 (b2 +. 1.0) 0 
+          if c2 >= 10000. then
+            loop (m-1) a2 (b2 +. 1.) 0. 0 
+          else
+          loop (m-1) a2 b2 (c2 +. 100.) 0 
     else
-      loop (i-1) a2 b2 tmp
+      loop (i-1) a2 b2 c2 tmp
   in
-  loop (m-1) 20000. (0.0) 0
+  loop (m-1) 10000000. (1000000.0) (0.) 0
            
 
 
@@ -43,7 +55,7 @@ let make_gragh lst =
 
 
 let _ = 
-  let file = open_in "correct_data_total_10to12.dat" in 
+  let file = open_in "fix_data10to12.dat" in 
   let rec loop tmp = 
     let a, b, c = Scanf.sscanf (input_line file) "%d %f %f " (fun a b c -> (a,b,c)) in 
     let tmp = (a,b,c)::tmp in
